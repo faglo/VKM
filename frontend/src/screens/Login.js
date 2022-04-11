bd98e5f64e008297285f63fde4c5115427984a5e
Faglo <bkom01@icloud.com>
2022-04-12 00:07:02 +0300

    Add action handlers

--- frontend/src/screens/Login.js
+++ frontend/src/screens/Login.js
@@ -7,10 +7,30 @@ function Login(){
     const [password, setPassword] = React.useState("");
     const [error, setError] = React.useState("");
     const [isLoading, setIsLoading] = React.useState(false);
-    const [captcha, setCaptcha] = React.useState("");
+    const [captcha, setCaptcha] = React.useState(null);
     const [captchaValue, setCaptchaValue] = React.useState("");
     const [twoFactorCode, setTwoFactorCode] = React.useState("");
     const [isTwoFactor, setIsTwoFactor] = React.useState(false);
+
+    const onLogin = () => {
+        console.log(login, password);
+    }
+
+    const onCaptchaSubmit = () => {
+        console.log(captchaValue);
+    }
+
+    const onTwoFactorSubmit = () => {
+        console.log(twoFactorCode);
+    }
+
+    const onCaptha = (captchaObj) => {
+        setCaptcha(captchaObj);
+    }
+
+    const onTwoFactor = () => {
+        setIsTwoFactor(true);
+    }
 
     
     return(
@@ -29,6 +49,7 @@ function Login(){
                     <LoadingButton
                         loading={isLoading}
                         fullWidth
+                        onClick={onLogin}
                     >Sign in</LoadingButton>
                 </Grid>
             </Grid>
@@ -41,7 +62,7 @@ function Login(){
                 severity="error"
             />
 
-            <Dialog maxWidth="xs" open={captcha.length > 0}>
+            <Dialog maxWidth="xs" open={captcha === null}>
                 <DialogTitle>Captcha required</DialogTitle>
                 <DialogContent>
                     <Grid container justify="center" alignItems="center" spacing={3}>
@@ -52,7 +73,7 @@ function Login(){
                             <TextField placeholder="Captcha" variant="outlined" fullWidth value={captchaValue} onChange={(e) => setCaptchaValue(e.target.value)}/>
                         </Grid>
                         <Grid item xs={12}>
-                            <Button variant="contained" fullWidth disabled={captchaValue.length === 0}>Submit</Button>
+                            <Button variant="contained" fullWidth disabled={captchaValue.length === 0} onClick={onCaptchaSubmit}>Submit</Button>
                         </Grid>
                         <Grid item xs={12}>
                             <Button variant="contained" fullWidth onClick={() => {setCaptcha(""); setIsLoading(false)}}>Cancel</Button>
@@ -69,7 +90,7 @@ function Login(){
                             <TextField placeholder="Code" variant="outlined" fullWidth value={twoFactorCode} onChange={(e) => setTwoFactorCode(e.target.value)}/>
                         </Grid>
                         <Grid item xs={12}>
-                            <Button variant="contained" fullWidth disabled={twoFactorCode.length === 0}>Submit</Button>
+                            <Button variant="contained" fullWidth disabled={twoFactorCode.length === 0} onClick={onTwoFactorSubmit}>Submit</Button>
                         </Grid>
                         <Grid item xs={12}>
                             <Button variant="contained" fullWidth onClick={() => {setIsTwoFactor(false); setIsLoading(false)}}>Cancel</Button>
