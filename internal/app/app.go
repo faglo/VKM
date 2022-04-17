98ab8b1a41d3ad4335c255a716c464637b421982
Max Foxie <maxunof@pm.me>
2022-04-17 16:31:04 +0300

    Temporarily don't play shit

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,11 +2,9 @@ package app
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"github.com/faglo/vkm/internal/player"
-	"github.com/faglo/vkm/internal/vkapi"
 	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
@@ -26,25 +24,24 @@ func (a *Application) Startup(ctx context.Context) {
 	a.player = player.New()
 
 	go a.playerEvents()
-	go func() {
-		time.Sleep(time.Second * 5)
+	// go func() {
+	// 	time.Sleep(time.Second * 5)
 
-		audios, err := vkapi.NewClient(
-			"a42780eec8417e41a8aaa6178237768934b271c32b8d52a54f74ffdc3db726f043b0eee4fd63ac8d49450").
-			AudioByID("-2001026462_108026462")
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+	// 	audios, err := vkapi.NewClient(
+	// 		"a42780eec8417e41a8aaa6178237768934b271c32b8d52a54f74ffdc3db726f043b0eee4fd63ac8d49450").
+	// 		AudioByID("-2001026462_108026462")
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 		return
+	// 	}
 
-		fmt.Println(a.player.Play(audios[0].URL))
-	}()
+	// 	a.player.Play(audios[0].URL)
+	// }()
 }
 
 func (a *Application) playerEvents() {
 	for {
 		pos := a.player.Position()
-		fmt.Println(pos)
 		runtime.EventsEmit(a.ctx, "player_position", pos)
 		time.Sleep(time.Millisecond * 500)
 	}
